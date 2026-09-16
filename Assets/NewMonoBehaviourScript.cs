7f247aec49b8107dd1513e47b85c7f45c74eb3e3
hwimin144 <zabo0101@naver.com>
2026-09-16 17:54:21 +0900

    VariableExample

--- Assets/NewMonoBehaviourScript.cs
+++ Assets/NewMonoBehaviourScript.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class VariableExample : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    int a;
     void Start()
     {
-        
-    }
+        a = 1;
+        Debug.Log(a);
+            // Debug.Log("Hellow World");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
+
+    
